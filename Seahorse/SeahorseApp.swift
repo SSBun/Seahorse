9b764fe2c03361b27ec44d17b016ee04751718f2
CSL <caishilin@zhihu.com>
2025-11-14 09:45:21 +0800

    Initial Commit

---
+++ Seahorse/SeahorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeahorseApp.swift
+//  Seahorse
+//
+//  Created by caishilin on 2025/11/14.
+//
+
+import SwiftUI
+
+@main
+struct SeahorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
